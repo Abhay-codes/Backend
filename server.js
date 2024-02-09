e443631a401c31e0cc180edf04cc74eedf77fce7
Abhay-codes <abhaysingh002001@gmail.com>
2024-02-09 11:55:33 +0530

    add  cookie-parser in server.js

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express =require('express');
 const cors =require('cors')
 const router =require("./routes")
 const database = require("./repositories/connectDb");
-
+const cookieParser=require('cookie-parser');
 
 const app =express()
 app.use(express.json()) //Without this middleware, you would need to manually parse the JSON using something like JSON.parse(req.body).
 
 // Configure CORS
-app.use(
-    cors({
-      origin: "http://localhost:3000",
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      exposedHeaders: ["Content-Length", "Authorization"],
-      credentials: true,
-    })
-  );
+app.use(cookieParser());
+app.use(cors({
+    origin: function (origin, callback) {
+        return callback(null, true);
+    },
+    optionsSuccessStatus: 200,
+    credentials: true
+}));
 
 const Port =8000;
 database;
